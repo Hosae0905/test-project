271dda3c113b7ad696455503749c7d99c98ff29f
Hosae0905 <didghtls11@gmail.com>
2024-01-12 22:43:19 +0900

    [Init] 프로젝트 생성

---
+++ src/main/java/com/example/miniprojecttest/MiniprojecttestApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniprojecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojecttestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprojecttestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniprojecttest/MiniprojecttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniprojecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojecttestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
